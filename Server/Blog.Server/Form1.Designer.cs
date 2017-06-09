0c8fe13212261f796ae7b051b94c8f3f56b8ecdc
Jakub <asmax6@gmail.com>
2017-06-09 22:50:43 +0200

    #server [add] AsynchronousSocketListener, PacketAnalyze

--- Server/Blog.Server/Form1.Designer.cs
+++ Server/Blog.Server/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.clientBox = new System.Windows.Forms.ListBox();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(524, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // clientBox
+            // 
+            this.clientBox.FormattingEnabled = true;
+            this.clientBox.Location = new System.Drawing.Point(524, 12);
+            this.clientBox.Name = "clientBox";
+            this.clientBox.Size = new System.Drawing.Size(145, 264);
+            this.clientBox.TabIndex = 3;
+            // 
+            // logBox
+            // 
+            this.logBox.FormattingEnabled = true;
+            this.logBox.Location = new System.Drawing.Point(12, 12);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(506, 264);
+            this.logBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 412);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.clientBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox clientBox;
+        public  System.Windows.Forms.ListBox logBox;
     }
 }
 
--- Server/Blog.Server/Form1.cs
+++ Server/Blog.Server/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,20 @@ namespace Blog.Server
 {
     public partial class Form1 : Form
     {
+        AsynchronousSocketListener asynchronousSocketListener;
         public Form1()
         {
             InitializeComponent();
+            asynchronousSocketListener = new AsynchronousSocketListener(logBox, clientBox);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => AsynchronousSocketListener.StartListening());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
         }
     }
 }
