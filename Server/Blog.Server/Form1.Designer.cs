a1b276613b8d491c1f5bbbdb292f551ff90a4dce
Jakub Rycyk <Jakub Rycyk>
2017-06-10 08:37:11 +0200

    #server [add] color winform

--- Server/Blog.Server/Form1.Designer.cs
+++ Server/Blog.Server/Form1.Designer.cs
@@ -36,51 +36,57 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Location = new System.Drawing.Point(12, 349);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.button2.Location = new System.Drawing.Point(524, 349);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 51);
             this.button2.TabIndex = 1;
             this.button2.Text = "Stop";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // clientBox
             // 
+            this.clientBox.BackColor = System.Drawing.SystemColors.Info;
             this.clientBox.FormattingEnabled = true;
-            this.clientBox.Location = new System.Drawing.Point(524, 12);
+            this.clientBox.Location = new System.Drawing.Point(522, 12);
             this.clientBox.Name = "clientBox";
-            this.clientBox.Size = new System.Drawing.Size(145, 264);
+            this.clientBox.Size = new System.Drawing.Size(147, 303);
             this.clientBox.TabIndex = 3;
             // 
             // logBox
             // 
+            this.logBox.BackColor = System.Drawing.SystemColors.Info;
             this.logBox.FormattingEnabled = true;
             this.logBox.Location = new System.Drawing.Point(12, 12);
             this.logBox.Name = "logBox";
-            this.logBox.Size = new System.Drawing.Size(506, 264);
+            this.logBox.Size = new System.Drawing.Size(504, 303);
             this.logBox.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(681, 412);
             this.Controls.Add(this.logBox);
             this.Controls.Add(this.clientBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Blog Server";
             this.ResumeLayout(false);
 
         }
