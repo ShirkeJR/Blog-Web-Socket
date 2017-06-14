651986da2e347e18d88351fdbca8bf0cd904bc8b
JachuPL <bonkowskijan@gmail.com>
2017-06-14 18:02:06 +0200

    Refactored PacketAnalyzeService to use constants definitions

--- Server/Blog.Server/service/PacketAnalyzeService.cs
+++ Server/Blog.Server/service/PacketAnalyzeService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Blog.Constants;
+using System;
 using System.Threading.Tasks;
 
 namespace Blog.Server
 {
-    class PacketAnalyzeService
+    internal class PacketAnalyzeService
     {
         #region Singleton
 
@@ -36,232 +34,198 @@ namespace Blog.Server
             string[] packet = content.Split('\t');
             int packetSize = Convert.ToInt32(packet[0]);
             string packetType = packet[1];
-            string messagee;
+            string returnMessage;
             switch (packetType)
             {
-                case "STATUS": // testowe
-                    messagee = await buildStatus(packet);
+                case StringConstants.RegisterPacketName:
+                    returnMessage = await buildRegister(packet);
                     break;
-                case "REGISTER":
-                    messagee = await buildRegister(packet);
+
+                case StringConstants.LoginPacketName:
+                    returnMessage = await buildLogin(packet, clientData);
                     break;
-                case "LOGIN":
-                    {
-                        messagee = await buildLogin(packet);
-                        if (messagee.Contains("OK"))
-                        {
-                            string[] paramList = messagee.Split('\t');
-                            clientData.Id = Convert.ToInt32(paramList[2]);
-                        }
-                    }
-                    break; 
-                case "DISPLAY_BLOGS":
-                    messagee = await buildDisplayBlogs(packet);
+
+                case StringConstants.DisplayBlogsPacketName:
+                    returnMessage = await buildDisplayBlogs(packet);
                     break;
-                case "DISPLAY_BLOG":
-                    messagee = await buildDisplayBlog(packet);
+
+                case StringConstants.DisplayBlogPacketName:
+                    returnMessage = await buildDisplayBlog(packet);
                     break;
-                case "ADD_ENTRY":
-                    messagee = await buildAddEntry(packet, clientData);
+
+                case StringConstants.AddEntryPacketName:
+                    returnMessage = await buildAddEntry(packet, clientData);
                     break;
-                case "DISPLAY_ENTRY":
-                    messagee = await buildDisplayEntry(packet);
+
+                case StringConstants.DisplayEntryPacketName:
+                    returnMessage = await buildDisplayEntry(packet);
                     break;
-                case "DELETE_ENTRY":
-                    messagee = await buildDeleteEntry(packet, clientData);
+
+                case StringConstants.DeleteEntryPacketName:
+                    returnMessage = await buildDeleteEntry(packet, clientData);
                     break;
-                case "CHANGE_BLOG_NAME":
-                    messagee = await buildChangeBlogName(packet, clientData);
+
+                case StringConstants.ChangeBlogNamePacketName:
+                    returnMessage = await buildChangeBlogName(packet, clientData);
                     break;
-                case "THX_BYE":
-                    messagee = await buildLogout(packet, clientData);
+
+                case StringConstants.LogoutPacketName:
+                    returnMessage = buildLogout(packet, clientData);
                     break;
-                case "EOT":
-                    messagee = await buildExit(packet);
+
+                case StringConstants.ConnectionClosePacketName:
+                    returnMessage = buildExit(packet);
                     break;
+
+                case StringConstants.PingPacketName:
+                    returnMessage = "PONG";
+                    break;
+
                 default:
-                    messagee = await buildDefault(packet);
+                    returnMessage = buildDefault(packet);
                     break;
             }
-            messagee = messagee + "\t";
-            content = messagee.Length + "\t" + messagee + "/rn/rn/rn$$";
+            //returnMessage = returnMessage + "\t";
+            //content = returnMessage.Length + "\t" + returnMessage + "/rn/rn/rn$$";
+            content = string.Format(StringConstants.GlobalPacketFormat, returnMessage.Length + 1, returnMessage, StringConstants.PacketEnding);
             return content;
-        }
-
-        private async Task<string> buildStatus(string[] packet) //testowe
-        {
-            string type = "OK";
-            return type + "\t" + "status is ok";
         }
 
         private async Task<string> buildRegister(string[] packet)
         {
-            string type = "REGISTER";
-            string param;
-            if (await AccountService.Instance.Register(packet[2], packet[3])) //ok
-            {
-                param = "OK";
-            }
-            else
-            {
-                param = "INVALID";
-            }
-            return type + "\t" + param;
+            string param = (await AccountService.Instance.Register(packet[2], packet[3])) ? StringConstants.RegisterPacketAnswerOK : StringConstants.RegisterPacketAnswerInvalid;
+
+            return string.Format(StringConstants.RegisterPacketAnswerFormat, StringConstants.RegisterPacketName, param);
         }
 
-
-        private async Task<string> buildLogin(string[] packet)
+        private async Task<string> buildLogin(string[] packet, ClientData clientData)
         {
-            string type = "LOGIN";
-            string param1 = "", param2= "";
-            int id;
-            if ((id = await AccountService.Instance.Login(packet[2], packet[3])) > 0) //exist
+            string param1 = string.Empty, param2 = string.Empty;
+            int id = -1;
+            if ((id = await AccountService.Instance.Login(packet[2], packet[3])) > 0) // jeśli istnieje i prawidłowe logowanie
             {
-                param1 = "OK";
+                param1 = StringConstants.LoginPacketAnswerOK;
                 param2 = Convert.ToString(id);
+                clientData.Id = id;
             }
-            else if (id < 0) //wrong data
+            else if (id < 0) // nieprawidłowe dane
             {
-                param1 = "FAILED";
-                param2 = "INVALID";
+                param1 = StringConstants.LoginPacketAnswerFailed;
+                param2 = StringConstants.LoginPacketAnswerFailedInvalid;
             }
-            else if (false) // może kiedyś locked
+            else if ((await AccountService.Instance.IsLocked(packet[2]))) // zablokowane
             {
-                param1 = "FAILED";
-                param2 = "LOCKED";
+                param1 = StringConstants.LoginPacketAnswerFailed;
+                param2 = StringConstants.LoginPacketAnswerFailedLocked;
             }
-            return type + "\t" + param1 + "\t" + param2;
+            return string.Format(StringConstants.LoginPacketAnswerFormat, StringConstants.LoginPacketName, param1, param2);
         }
 
         private async Task<string> buildDisplayBlogs(string[] packet)
         {
-            string type = "DISPLAY_BLOGS";
             string paramsList = await BlogService.Instance.DisplayBlogs();
             if (paramsList.Length > 2)
-                return type + "\t" + paramsList;
+                return string.Format(StringConstants.DisplayBlogsPacketAnswerFormat, StringConstants.DisplayBlogsPacketName, paramsList);
             else
-                return type;
+                return StringConstants.DisplayBlogsPacketName;
         }
-    
+
         private async Task<string> buildDisplayBlog(string[] packet)
         {
-            string type = "DISPLAY_BLOG";
             int id = Convert.ToInt32(packet[2]);
-            if (await BlogService.Instance.BlogExists(id))// Jeżeli blog X istnieje
+            if (await BlogService.Instance.Exists(id))// Jeżeli blog X istnieje
             {
                 string paramsList = await BlogService.Instance.DisplayBlogPosts(id); //lista wpisów w blogu
                 if (paramsList.Length > 2)
-                    return type + "\t" + paramsList;
+                    return string.Format(StringConstants.DisplayBlogPacketAnswerFormat, StringConstants.DisplayBlogPacketName, paramsList);
                 else
-                    return type;
+                    return StringConstants.DisplayBlogPacketName;
             }
             else
-            {
-                string param1 = "FAILED";
-                return type + "\t" + param1;
-            }
+                return string.Format(StringConstants.DisplayBlogPacketAnswerFormat, StringConstants.DisplayBlogPacketName, StringConstants.DisplayBlogPacketAnswerFailed);
         }
 
         private async Task<string> buildAddEntry(string[] packet, ClientData clientData)
         {
-            string type = "ADD_ENTRY";
-            string param1;
-            if (clientData.Id != -1)// Jak stworzył
+            string param1 = string.Empty, param2 = string.Empty;
+            if (clientData.Id != -1) // zalogowany
             {
-                if(await BlogService.Instance.AddEntry(clientData.Id, packet[2], packet[3]))
+                if (await BlogService.Instance.AddEntry(clientData.Id, packet[2], packet[3]))
                 {
-                    param1 = "OK";
-                    string param2 = Convert.ToString(await BlogService.Instance.GetEntryId(clientData.Id, packet[2]));
-                    return type + "\t" + param1 + "\t" + param2;
+                    param1 = StringConstants.AddEntryPacketAnswerOK;
+                    param2 = Convert.ToString(await BlogService.Instance.GetEntryId(clientData.Id, packet[2]));
                 }
-                else if (packet[2].Length > 31) // zły tytuł
+                else if (packet[2].Length > Int16Constants.BlogPostTitleMaxLength) // zły tytuł
                 {
-                    param1 = "ERR_TITLE";
-                    return type + "\t" + param1;
+                    param1 = StringConstants.AddEntryPacketAnswerInvalid;
+                    param2 = StringConstants.AddEntryPacketAnswerInvalidTitle;
                 }
-                else if (packet[3].Length > 2047) // zła treść
+                else if (packet[3].Length > Int16Constants.BlogPostContentMaxLength) // zła treść
                 {
-                    param1 = "ERR_CONTENT";
-                    return type + "\t" + param1;
+                    param1 = StringConstants.AddEntryPacketAnswerInvalid;
+                    param2 = StringConstants.AddEntryPacketAnswerInvalidContent;
                 }
             }
-            param1 = "ERR_OWNER";
-            return type + "\t" + param1;
-        }
+            else return StringConstants.LoginRequiredAnswer;
 
+            return string.Format(StringConstants.AddEntryPacketAnswerFormat, StringConstants.AddEntryPacketName, param1, param2);
+        }
 
         private async Task<string> buildDisplayEntry(string[] packet)
         {
-            string type = "DISPLAY_ENTRY";
             int id = Convert.ToInt32(packet[2]);
-            string entry = await BlogService.Instance.DisplayEntry(id); //lista wpisów w blogu
-            return type + "\t" + entry;
+            string entry = await BlogService.Instance.DisplayEntry(id); // wpis w blogu
+            return string.Format(StringConstants.DisplayEntryPacketFormat, StringConstants.DisplayEntryPacketName, entry);
         }
 
         private async Task<string> buildDeleteEntry(string[] packet, ClientData clientData)
         {
-            string type = "DELETE_ENTRY";
-            string param1, param2;
+            string param1 = StringConstants.DeleteEntryPacketAnswerFailed,
+                param2 = StringConstants.DeleteEntryPacketAnswerFailedNotOwner;
             int id = Convert.ToInt32(packet[2]);
             if (clientData.Id != -1) // UDAŁO SIĘ
             {
-                if(await BlogService.Instance.DeleteEntry(id, clientData.Id))
+                if (await BlogService.Instance.DeleteEntry(id, clientData.Id))
                 {
-                    param1 = "OK";
+                    param1 = StringConstants.DeleteEntryPacketAnswerOK;
                     param2 = Convert.ToString(id);
                 }
                 else
                 {
-                    param1 = "FAILED ";
-                    param2 = "NOTEXIST";
+                    param1 = StringConstants.DeleteEntryPacketAnswerFailed;
+                    param2 = StringConstants.DeleteEntryPacketAnswerFailedNotExist;
                 }
-                return type + "\t" + param1 + "\t" + param2;
             }
-            param1 = "FAILED";
-            param2 = "NOTOWNER";
-            return type + "\t" + param1 + "\t" + param2;
+            return string.Format(StringConstants.DeleteEntryPacketAnswerFormat, StringConstants.DeleteEntryPacketName, param1, param2);
         }
 
         private async Task<string> buildChangeBlogName(string[] packet, ClientData clientData)
         {
-            string type = "CHANGE_BLOG_NAME";
             int id = Convert.ToInt32(packet[2]);
             string newTitle = packet[3];
-            string param = "";
+            string param = StringConstants.ChangeBlogNamePacketAnswerFailed;
             if (clientData.Id != id)
-            {
-                param = "NOTOWNER";
-                return type + "\t" + param;
-            }
-            if (await BlogService.Instance.ChangeBlogName(clientData.Id, id, newTitle)) // UDAŁO SIĘ
-            {
-                param = "OK";
-                return type + "\t" + param;
-            }
-            param = "FAILED";
-            return type + "\t" + param;
+                param = StringConstants.ChangeBlogNamePacketAnswerNotOwner;
+            else if (await BlogService.Instance.ChangeBlogName(clientData.Id, id, newTitle))
+                param = StringConstants.ChangeBlogNamePacketAnswerOK;
+
+            return string.Format(StringConstants.ChangeBlogNamePacketAnswerFormat, StringConstants.ChangeBlogNamePacketName, param);
         }
 
-
-        private async Task<string> buildLogout(string[] packet, ClientData clientData)
+        private string buildLogout(string[] packet, ClientData clientData)
         {
-            string type = "THX_BYE";
             clientData.Id = -1;
-            return type;
+            return StringConstants.LogoutPacketName;
         }
 
-        private async Task<string> buildDefault(string[] packet)
+        private string buildDefault(string[] packet)
         {
-            string type = "QUE?";
-            return type;
+            return StringConstants.UnrecognizedCommandAnswer;
         }
 
-        private async Task<string> buildExit(string[] packet)
+        private string buildExit(string[] packet)
         {
-            string type = "EOT";
-            return type;
+            return StringConstants.ConnectionClosePacketName;
         }
-
     }
-
 }
