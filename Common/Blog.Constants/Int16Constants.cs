1d4c474f7d725c1edaf08cf3f0a1ddab5621a66f
JachuPL <bonkowskijan@gmail.com>
2017-06-14 17:57:45 +0200

    Added constants definitions to share between client and server binaries

--- Common/Blog.Constants/Int16Constants.cs
+++ Common/Blog.Constants/Int16Constants.cs
@@ -3,5 +3,8 @@
     public static class Int16Constants
     {
         public const ushort DefaultPort = 11002;
+
+        public const ushort BlogPostTitleMaxLength = 32;
+        public const ushort BlogPostContentMaxLength = 2048;
     }
 }
