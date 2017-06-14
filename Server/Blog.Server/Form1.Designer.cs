e8edf13231869c4e151eef01cfce04892b7fc224
ShirkeJR <asmax6@gmail.com>
2017-06-14 08:09:48 +0200

    #server [fix] dizajn

--- Server/Blog.Server/Form1.Designer.cs
+++ Server/Blog.Server/Form1.Designer.cs
@@ -32,12 +32,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.clientBox = new System.Windows.Forms.ListBox();
             this.logBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(12, 349);
+            this.button1.Location = new System.Drawing.Point(12, 344);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 51);
             this.button1.TabIndex = 0;
@@ -49,7 +51,7 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button2.Location = new System.Drawing.Point(524, 349);
+            this.button2.Location = new System.Drawing.Point(703, 344);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 51);
             this.button2.TabIndex = 1;
@@ -60,27 +62,55 @@
             // clientBox
             // 
             this.clientBox.BackColor = System.Drawing.SystemColors.Info;
+            this.clientBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.clientBox.FormattingEnabled = true;
-            this.clientBox.Location = new System.Drawing.Point(522, 12);
+            this.clientBox.ItemHeight = 16;
+            this.clientBox.Location = new System.Drawing.Point(633, 32);
             this.clientBox.Name = "clientBox";
-            this.clientBox.Size = new System.Drawing.Size(147, 303);
+            this.clientBox.Size = new System.Drawing.Size(215, 308);
             this.clientBox.TabIndex = 3;
             // 
             // logBox
             // 
             this.logBox.BackColor = System.Drawing.SystemColors.Info;
+            this.logBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.logBox.FormattingEnabled = true;
-            this.logBox.Location = new System.Drawing.Point(12, 12);
+            this.logBox.ItemHeight = 16;
+            this.logBox.Location = new System.Drawing.Point(12, 32);
             this.logBox.Name = "logBox";
-            this.logBox.Size = new System.Drawing.Size(504, 303);
+            this.logBox.Size = new System.Drawing.Size(614, 308);
             this.logBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(241, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Logi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(684, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Użytkownicy";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(681, 412);
+            this.ClientSize = new System.Drawing.Size(859, 407);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.logBox);
             this.Controls.Add(this.clientBox);
             this.Controls.Add(this.button2);
@@ -88,6 +118,7 @@
             this.Name = "Form1";
             this.Text = "Blog Server";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,6 +128,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox clientBox;
         public  System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
